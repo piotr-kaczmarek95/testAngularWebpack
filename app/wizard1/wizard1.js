142bbe1c2415b0d97643f3a617c2f4927eb08e6b
Piotr <piotr.kaczmarek@yandex.com>
2019-10-28 13:47:27 +0100

    pełnoekranowy wizard po przebudowaniu

--- app/wizard1/wizard1.js
+++ app/wizard1/wizard1.js
@@ -29,78 +29,33 @@ angular.module('homeworkProject.wizard1', ['ngRoute'])
 
         console.log("Hello world!");
 
-        $scope.lockFirstWindow = true;
-        $scope.lockSecondWindow = true;
-        // $scope.option1 = false;
-        // $scope.option2 = false;
-        // $scope.option3 = false;
-
         $scope.storedData = {};
 
         let key = "user";
 
         $scope.next = function () {
 
-            console.log($location.path()); //pokazuje aktualna lokalizacje
-            // console.log(typeof ($location.path())); //string
+            console.log($location.path()); //pokazuje aktualna lokalizacje - typem jest string
 
             ///Pobieranie zrealizowane juz w funkcji reload/////
             // if (localStorageService.get(key) != null) $scope.storedData = localStorageService.get(key); //zawsze pobieraj?      
 
             let nextIndex = parseInt($location.path().charAt($location.path().length - 1)) + 1; //inkrementacja indeksu
 
-            $location.path("/wizard" + nextIndex); //przejscie do kolejnego okna wizarda
-
-            // if (nextIndex == 2) {
-
-            //     // console.log($scope.name, $scope.surname);
-
-            //     // localStorageService.set("userName", $scope.name);
-            //     // localStorageService.set("userSurname", $scope.surname);
-
-            //     //pobranie ze storage obiektu z obecnie wpisanymi danymi
-            //     // if ($scope.storedData) {
-            //     //     $scope.storedData = localStorageService.get(key);
-            //     // }
-            //     // dopisanie własności
-
-            //     //niepotrzebnie, bo już tak nazywam?
-
-            //     // $scope.storedData.userName = $scope.name;
-            //     // $scope.storedData.userSurname = $scope.surname;
-
-            //     //zapisanie do storage
-            //     localStorageService.set(key, $scope.storedData);
-            // }
-
-            // if (nextIndex == 3) {
-
-            //     // localStorageService.set("userAge", $scope.age);
-
-
-            //     //pobranie ze storage obiektu z obecnie wpisanymi danymi
-            //     // if ($scope.storedData) {
-            //     //     $scope.storedData = localStorageService.get(key);
-            //     // }
-            //     // dopisanie własności
-            //     // $scope.storedData.userAge = $scope.age;
-            //     //zapisanie do storage              
-
-            //     localStorageService.set(key, $scope.storedData);
-
-            // }
-
             if (nextIndex == 4) {
-           
-                //gdy niekliknięte ustawiam na false zamiast undefined
+
+                //gdy checkboksy niekliknięte ustawiam na false zamiast undefined
                 if ($scope.storedData.option1 == undefined) $scope.storedData.option1 = false;
                 if ($scope.storedData.option2 == undefined) $scope.storedData.option2 = false;
                 if ($scope.storedData.option3 == undefined) $scope.storedData.option3 = false;
-                // localStorageService.set(key, $scope.storedData);
 
             }
 
+            //kazdy krok uaktualnia storedData o dane wprowadzone w inputach i checkboksach - zapisuje tak zmieniony obiekt w local storage
+
             localStorageService.set(key, $scope.storedData);
+
+            $location.path("/wizard" + nextIndex); //przejscie do kolejnego okna wizarda 
         }
 
         $scope.prev = function () {
@@ -115,38 +70,7 @@ angular.module('homeworkProject.wizard1', ['ngRoute'])
             $location.path("/view1");
 
             localStorageService.remove(key);
-        }
-
-        $scope.tooYoung = function () {
-
-            if ($scope.storedData.userAge != undefined) {
-
-                if ($scope.storedData.userAge < 15) {
-                    $scope.lockSecondWindow = true;
-                } else {
-                    $scope.lockSecondWindow = false;
-                }
-
-            }
-        }
-
-        $scope.tooShort = function () {
-
-            console.log("Zmiana wykryta!");
-
-            // zabezpieczenie minimalnej dlugosci imienia i nazwiska
-
-            if ($scope.storedData.userName != undefined && $scope.storedData.userName.length > 1 && $scope.storedData.userSurname != undefined && $scope.storedData.userSurname.length > 1) {
-
-                $scope.lockFirstWindow = false;
-            } else {
-
-                $scope.lockFirstWindow = true;
-            }
-
-            console.log($scope.lockFirstWindow);
-
-        }
+        }       
 
         $scope.summarise = function () {
 
@@ -163,44 +87,18 @@ angular.module('homeworkProject.wizard1', ['ngRoute'])
             // $scope.abort();
         }
 
-        //przejcie do czwartego ekranu wizarda powoduje zebranie wprowadzonych danych do pojedynczego obiektu w local storage i wyswietlenie 
-
-        //przeniesione do funkcji reload
-
-        // if ($location.path() == "/wizard4") {
-
-        //     $scope.summarise();
-        // }
-
         $scope.reload = function () {
 
-            //przy kazdym zaladowaniu widoku - nieistotne czy spowodowalo go klikniecie prev czy next - pobranie obiektu z local storage
+            //przy kazdym zaladowaniu widoku - nieistotne czy spowodowalo je klikniecie prev czy next - pobranie obiektu z local storage
 
             //dzieki temu przy cofaniu pola beda wypelnione wartosciami podanymi wczesniej przez uzytkownika
 
-            if (localStorageService.get(key) != null){
+            if (localStorageService.get(key) != null) {
 
-                $scope.storedData = localStorageService.get(key); //pobranie akutalnej zawartosci danych z local storage i przypisanie do obiektu, na ktorym operuja inputy
+                $scope.storedData = localStorageService.get(key); //pobranie akutalnej zawartosci danych z local storage i przypisanie do obiektu, na ktorego polach operuja inputy
                 console.log("Pobrane stored data");
                 console.log($scope.storedData);
 
-            }        
-
-            if ($location.path() == "/wizard1") {
-
-
-                $scope.tooShort(); //gdy cofam sie ze slajdu 2 - automatyczne sprawdzenie (a nie przy zmianie tekstu w inpucie)
-
-            }
-
-            if ($location.path() == "/wizard2") {
-
-                $scope.tooYoung();
-            }
-
-            if ($location.path() == "/wizard3") {
-
-            
             }
 
             if ($location.path() == "/wizard4") {
@@ -210,9 +108,5 @@ angular.module('homeworkProject.wizard1', ['ngRoute'])
         }
 
         $scope.reload(); //przy kazdym przeladowaniu
-
-
-
-
 
     }])
